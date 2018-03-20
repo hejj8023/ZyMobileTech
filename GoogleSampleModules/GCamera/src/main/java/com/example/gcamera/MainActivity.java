54f296428bb3758eb3d9c0d8ceee730a18145e75
xfgczzg <xfgczzg@163.com>
2018-03-20 22:25:50 +0800

    google camera sample项目工程创建

--- GoogleSampleModules/GCamera/src/main/java/com/example/gcamera/MainActivity.java
+++ GoogleSampleModules/GCamera/src/main/java/com/example/gcamera/MainActivity.java
@@ -3,11 +3,32 @@ package com.example.gcamera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.common.corel.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentViewId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void addListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected PermissionListener getPermissonCallBack() {
+        return null;
     }
 }
