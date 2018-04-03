62a22d383c634e5b39e0a547197761e7b9379a70
xfgczzg <xfgczzg@163.com>
2018-04-03 19:11:17 +0800

    效果图片添加

---
+++ LibModules/commonlib/src/main/java/com/example/glide/GlideModule.java
@@ -0,0 +1,11 @@
+package com.example.glide;
+
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by ubt on 2018/4/3.
+ */
+
+@com.bumptech.glide.annotation.GlideModule
+public class GlideModule extends AppGlideModule {
+}
--- ToolAppModule/BitmapAssistant/src/main/java/com/example/bmpa/MainActivity.java
+++ ToolAppModule/BitmapAssistant/src/main/java/com/example/bmpa/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.bmpa;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
@@ -8,19 +7,21 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.graphics.Typeface;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.common.corel.BaseActivity;
+import com.example.glide.GlideApp;
+import com.example.glide.GlideCircleTransform;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
@@ -29,6 +30,9 @@ public class MainActivity extends BaseActivity {
 
     private String sourFileName = "test_source.png";
     private File sourceFile;
+
+    @BindView(R.id.image_view)
+    ImageView imageView;
 
     @Override
     protected int getContentViewId() {
@@ -64,10 +68,10 @@ public class MainActivity extends BaseActivity {
 
     @OnClick({R.id.btn_create_bmp, R.id.btn_sobmp_scale_target,
             R.id.btn_sobmp_crop_target, R.id.btn_scbmp_crop_target,
-            R.id.btn_sobmp_crop_target_by_brd, R.id.btn_scbmp_crop_target_by_brd
+            R.id.btn_sobmp_crop_target_by_brd, R.id.btn_scbmp_crop_target_by_brd,
+            R.id.btn_load_cicle_by_glide
     })
     public void onViewClick(View view) {
-
         switch (view.getId()) {
             case R.id.btn_create_bmp:
                 createSourceFile();
@@ -140,7 +144,34 @@ public class MainActivity extends BaseActivity {
                     }
                 }
                 break;
+            case R.id.btn_load_cicle_by_glide:
+                File tmpFile = new File(firDir, "test_brd_scale_700x700.png");
+                if (tmpFile.exists()) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(tmpFile.getAbsolutePath());
+                    try {
+                        BitmapRegionDecoder regionDecoder = BitmapRegionDecoder.newInstance(tmpFile.getAbsolutePath(), true);
+                        int h = bitmap.getHeight() / 2;
+                        int w = bitmap.getWidth() / 2;
+                        Rect react = new Rect();
+                        int baseLine = 200 / 2;
+                        react.left = w - baseLine;
+                        react.top = h - baseLine;
+                        react.right = w + baseLine;
+                        react.bottom = h + baseLine;
+                        Bitmap bitmap1 = regionDecoder.decodeRegion(react, null);
 
+                        // TODO: 2018/4/3 使用glide加载circle图片
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        bitmap1.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                        byte[] bytes = baos.toByteArray();
+                        GlideCircleTransform circleTransform = new GlideCircleTransform(mContext);
+                        GlideApp.with(mContext).load(bytes).transform(circleTransform).into
+                                (imageView);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
         }
     }
 
