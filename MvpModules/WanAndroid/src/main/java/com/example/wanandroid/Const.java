c377a18e3609a1c1809375e275eb79710eb23692
xfgczzg <xfgczzg@163.com>
2018-04-09 19:54:47 +0800

    玩转andorid项目添加

---
+++ MvpModules/WanAndroid/src/main/java/com/example/wanandroid/Const.java
@@ -0,0 +1,12 @@
+package com.example.wanandroid;
+
+/**
+ * Created by example on 2018/4/9.
+ */
+
+public class Const {
+    public static class USERINFO_KEY {
+        // 登录状态
+        public static final String IS_LOGIN = "mIsLogin";
+    }
+}
