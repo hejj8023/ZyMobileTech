7ba4aca66fc322bd53e008260811ff5c60d35a4b
xfgczzg <xfgczzg@163.com>
2018-04-09 19:56:03 +0800

    玩转andorid项目添加

---
+++ MvpModules/WanAndroid/src/main/java/com/example/wanandroid/mvp/contract/LoginContract.java
@@ -0,0 +1,45 @@
+package com.example.wanandroid.mvp.contract;
+
+import com.zhiyangstudio.commonlib.mvp.inter.IView;
+
+/**
+ * Created by example on 2018/4/9.
+ */
+
+public interface LoginContract {
+    interface IUserPresenter {
+        void login();
+
+        void register();
+    }
+
+    interface ILoginView extends IView {
+        /**
+         * 获取用户名
+         *
+         * @return
+         */
+        String getUserName();
+
+        /**
+         * 获取密码
+         *
+         * @return
+         */
+        String getPassword();
+
+        /**
+         * 账号错误
+         *
+         * @param msg
+         */
+        void onAccountError(String msg);
+
+        /**
+         * 登录 或注册结果
+         *
+         * @param msg
+         */
+        void showResult(String msg);
+    }
+}
---
+++ MvpModules/WanAndroid/src/main/java/com/example/wanandroid/mvp/presenter/LoginPresenter.java
@@ -0,0 +1,21 @@
+package com.example.wanandroid.mvp.presenter;
+
+import com.example.wanandroid.mvp.contract.LoginContract;
+import com.zhiyangstudio.commonlib.mvp.presenter.BasePresenter;
+
+/**
+ * Created by example on 2018/4/9.
+ */
+
+public class LoginPresenter extends BasePresenter<LoginContract.ILoginView> implements
+        LoginContract.IUserPresenter {
+    @Override
+    public void login() {
+        
+    }
+
+    @Override
+    public void register() {
+
+    }
+}
