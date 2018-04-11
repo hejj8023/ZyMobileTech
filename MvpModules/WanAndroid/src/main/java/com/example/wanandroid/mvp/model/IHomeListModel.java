3c9be42ef9bfc027d4ddbe340ed699d955e90276
xfgczzg <xfgczzg@163.com>
2018-04-11 15:11:38 +0800

    玩转andorid首页功能实现，轮播图列表和数据列表数据获取

---
+++ MvpModules/WanAndroid/src/main/java/com/example/wanandroid/mvp/model/IHomeListModel.java
@@ -0,0 +1,24 @@
+package com.example.wanandroid.mvp.model;
+
+import com.example.wanandroid.bean.ArticleBean;
+import com.example.wanandroid.bean.BannerBean;
+import com.zhiyangstudio.commonlib.net.callback.RxConsumer;
+import com.zhiyangstudio.commonlib.net.callback.RxPageListObserver;
+
+import java.util.List;
+
+/**
+ * Created by ubt on 2018/4/11.
+ */
+
+public interface IHomeListModel {
+    /**
+     * 获取首页文章列表和Banner
+     *
+     * @param page
+     * @param consumer
+     * @param observer
+     */
+    void getHomeData(int page, RxConsumer<List<BannerBean>> consumer,
+                     RxPageListObserver<ArticleBean> observer);
+}
