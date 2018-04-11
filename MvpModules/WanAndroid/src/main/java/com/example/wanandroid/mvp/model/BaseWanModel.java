817267628a3b1d112b4c6ffa3491e17fffecfd41
xfgczzg <xfgczzg@163.com>
2018-04-11 15:10:46 +0800

    玩转andorid首页功能实现，轮播图列表和数据列表数据获取

---
+++ MvpModules/WanAndroid/src/main/java/com/example/wanandroid/mvp/model/BaseWanModel.java
@@ -0,0 +1,17 @@
+package com.example.wanandroid.mvp.model;
+
+import com.example.wanandroid.Const;
+import com.example.wanandroid.api.ApiServer;
+import com.zhiyangstudio.commonlib.mvp.model.BaseModel;
+
+/**
+ * Created by ubt on 2018/4/11.
+ */
+
+public class BaseWanModel extends BaseModel {
+    public ApiServer getApi() {
+        return createApiService(Const.URL_CONFIG.baseUrl, ApiServer.class);
+    }
+
+
+}
