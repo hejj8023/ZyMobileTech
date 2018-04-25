7271dd072e53cccce2baed830c7ed4c000f7b9f6
xfgczzg <xfgczzg@163.com>
2018-04-25 12:42:43 +0800

    美化版环境搭建

---
+++ MvpModules/WanAndroidAdvance/src/main/java/com/example/wav/AdvApp.java
@@ -0,0 +1,14 @@
+package com.example.wav;
+
+import com.zhiyangstudio.commonlib.corel.BaseApp;
+
+/**
+ * Created by zhiyang on 2018/4/25.
+ */
+
+public class AdvApp extends BaseApp {
+    @Override
+    protected String getLogTag() {
+        return "wanandroid";
+    }
+}
