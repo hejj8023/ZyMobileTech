c322b9cebce77dcde928dd717fdf16f6fac45b33
xfzzg <xfgczzg@163.com>
2018-05-20 15:24:48 +0800

    工程整理

--- AppModules/DaggerDemo/src/main/java/com/example/wav/mvp/model/FilterModel.java
+++
@@ -1,8 +0,0 @@
-package com.example.wav.mvp.model;
-
-/**
- * Created by zhiyang on 2018/4/26.
- */
-
-public class FilterModel extends BaseFilterDataModel {
-}
--- VideoPlayers/src/main/java/com/example/player/Const.java
+++
@@ -1,9 +0,0 @@
-package com.example.player;
-
-/**
- * Created by example on 2018/5/11.
- */
-
-public class Const {
-    public static final String VIDEO_DIR_PATH = "/mnt/sdcard/videos";
-}
