5b8264ef7a371f7b80c7e0b5877d7bb16981a2b3
xfgczzg <xfgczzg@163.com>
2018-06-01 19:57:04 +0800

    视频录制和声网demo项目创建

---
+++ AppModules/AgoraDemo/src/main/java/com/example/agorad/AgoraApp.java
@@ -0,0 +1,38 @@
+package com.example.agorad;
+
+
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.tencent.bugly.crashreport.CrashReport;
+import com.zhiyangstudio.commonlib.CommonConst;
+import com.zhiyangstudio.commonlib.corel.BaseApp;
+import com.zhiyangstudio.commonlib.utils.OkHttpUtils;
+import com.zhiyangstudio.commonlib.utils.PreUtils;
+
+/**
+ * Created by example on 2018/6/1.
+ */
+
+public class AgoraApp extends BaseApp {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashReport.initCrashReport(getApplicationContext(), "181fd73244", true);
+
+        CommonConst.NET_CACHE_DIR_NAME = "agora";
+        PreUtils.init("agora_config");
+        OkHttpUtils.isSupportDataInterceptor(true);
+    }
+
+    @Override
+    protected String getLogTag() {
+        return "agoraapp";
+    }
+}
