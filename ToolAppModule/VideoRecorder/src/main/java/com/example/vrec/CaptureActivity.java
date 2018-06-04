cb93c71825541cca9a7be37f618a3b9ea77a4ea6
xfgczzg <xfgczzg@163.com>
2018-06-04 12:12:17 +0800

    工程整理

---
+++ ToolAppModule/VideoRecorder/src/main/java/com/example/vrec/CaptureActivity.java
@@ -0,0 +1,14 @@
+package com.example.vrec;
+
+import com.zhiyangstudio.commonlib.corel.BaseToolbarSupportActivity;
+
+/**
+ * Created by example on 2018/6/4.
+ */
+
+public class CaptureActivity extends BaseToolbarSupportActivity {
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.actiivty_capture;
+    }
+}
