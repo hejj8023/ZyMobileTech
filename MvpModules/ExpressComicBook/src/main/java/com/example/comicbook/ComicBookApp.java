4f82ab0be90b17eaa69a38a0d0db3e82079f4f07
xfgczzg <xfgczzg@163.com>
2018-06-26 19:58:03 +0800

    ComicBook欢迎页功能实现

--- MvpModules/ExpressComicBook/src/main/java/com/example/comicbook/ComicBookApp.java
+++ MvpModules/ExpressComicBook/src/main/java/com/example/comicbook/ComicBookApp.java
@@ -1,13 +1,18 @@
 package com.example.comicbook;
 
 
-import android.app.Application;
-
+import com.tencent.bugly.crashreport.CrashReport;
 import com.zysdk.vulture.clib.corel.BaseApp;
 
 public class ComicBookApp extends BaseApp {
     @Override
     public void onCreate() {
         super.onCreate();
+        CrashReport.initCrashReport(getApplicationContext(), "bfbdfa9a27", true);
+    }
+
+    @Override
+    protected String getLogTag() {
+        return "ComicBook";
     }
 }
---
+++ MvpModules/ExpressComicBook/src/main/java/com/example/comicbook/ui/activity/HomeActivity.java
@@ -0,0 +1,12 @@
+package com.example.comicbook.ui.activity;
+
+import com.example.comicbook.R;
+import com.zysdk.vulture.clib.corel.BaseToolbarSupportActivity;
+
+public class HomeActivity extends BaseToolbarSupportActivity {
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_home;
+    }
+}
